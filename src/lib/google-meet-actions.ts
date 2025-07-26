f4944a9b4ae44c08fd578e8d143eccdda907242b
Mauro Marconetto <mauro.marconetto@gmail.com>
2025-07-26 22:51:43 +0000

    agregado de console log

--- src/lib/google-meet-actions.ts
+++ src/lib/google-meet-actions.ts
@@ -26,6 +26,7 @@ type ActionResult = {
 // Helper function to create an OAuth2 client
 function createOAuthClient(): OAuth2Client {
   const redirectUri = `${process.env.NEXT_PUBLIC_APP_URL}/api/oauth2callback`;
+  
   return new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
